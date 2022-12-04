a0a99a98430e946091e0694417b152bd7040f7d2
ThomasO328 <100553765+ThomasO328@users.noreply.github.com>
2022-12-03 20:17:29 -0800

    Show routes connected

--- controllers/places.js
+++ controllers/places.js
@@ -6,6 +6,25 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+// GET /places
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+  })
+
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', { place: places[id] })
+    }
+  })
+  
+
 router.post('/', (req, res) => {
     if (!req.body.pic) {
       // Default image if one is not provided
@@ -23,10 +42,7 @@ router.post('/', (req, res) => {
    
 
 
-// GET /places
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
-  })
+
   
 
 module.exports = router
